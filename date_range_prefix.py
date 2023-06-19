7eb412fcdb6f0802d0e3a6535639ed59df4e483d
randomseed42 <50793718+randomseed42@users.noreply.github.com>
2023-06-20 00:32:25 +0800

    updated after code review

--- date_range_prefix.py
+++ date_range_prefix.py
@@ -1,87 +1,145 @@
 from datetime import datetime
-import calendar
+from calendar import monthrange
+from typing import List, Literal, Optional
 
-def date_range_prefix(start=None, end=None):
+
+def month_days(year: int, month: int) -> int:
     """
-    Get list of date string as prefix between start and end, 
-    compress full month to %Y-%m, 
-    compress full year to %Y.
-    
-    Returned date string delimiter use forward slash, like folders.
-    
+    Get how many days in the month of the year.
+
+    Parameters
+    ----------
+    year : int
+        Integer of the year
+    month : int
+        Integer of the month
+
+    Returns
+    -------
+    int
+        Integer of how many days in the month of the year
+    """
+    return monthrange(year, month)[1]
+
+
+def is_month_last_day(date: datetime) -> bool:
+    """
+    Check if date is the last day of that month.
+
+    Parameters
+    ----------
+    date: datetime
+        Date to be checked
+
+    Returns
+    -------
+    bool
+        True if it is the last day of that month, else False
+    """
+    return date.day == month_days(date.year, date.month)
+
+
+def date_range_prefix(
+        start: Optional[str] = None,
+        end: Optional[str] = None,
+        input_delimiter: Literal['-', '/'] = '-',
+        output_delimiter: Literal['-', '/'] = '/',
+) -> List[str]:
+    """
+    By given the start date and end date,
+    get list of all dates in between them,
+    each date is in string format %Y/%m/%d,
+    compress full month to %Y/%m,
+    compress full year to %Y
+
     For example:
 
-    >>> date_range_prefix(start='2023-01-30', '2023-03-01')
-    ['2023/01/30', '2023/01/31', '2023/02', '2023-03-01']
+    >>> date_range_prefix(start='2023-01-30', end='2023-03-01')
+    ['2023/01/30', '2023/01/31', '2023/02', '2023/03/01']
 
-    >>> date_range_prefix(start='2023-01-01', '2023-03-31')
-    ['2023/01', '2023-02', '2023-03']
+    >>> date_range_prefix(start='2023/01/01', end='2023/03/31', input_delimiter='/', output_delimiter='-')
+    ['2023-01', '2023-02', '2023-03']
+
+    >>> date_range_prefix(start='2021-01-01', end='2021-12-31')
+    ['2021']
+
+    >>> date_range_prefix(start='2021-02-10')
+    ['2021/02/10']
 
     >>> date_range_prefix()
     ['2023/06/17']
 
-    >>> date_range_prefix('2021-01-01', '2021-12-31')
-    ['2021']
-    
+    Parameters
+    ----------
+    start : str
+        start date in string format, like %Y-%m-%d
+    end : str
+        end date in string format, like %Y-%m-%d
+    input_delimiter : Literal['-', '/'], default '-'
+        input start date and end date delimiter
+    output_delimiter : Literal['-', '/'], default '/'
+        output date prefixes delimiter
+
+    Returns
+    -------
+    list
+        List of date prefixes.
+        Each prefix is in string format, like %Y/%m/%d, or %Y/%m, or %Y.
+
     """
+
+    input_format = f'%Y{input_delimiter}%m{input_delimiter}%d'
+
     if start is None:
-        start = datetime.today().strftime('%Y-%m-%d')
+        start = datetime.today().strftime(input_format)
     if end is None:
         end = start
 
-    s = datetime.strptime(start, '%Y-%m-%d')
-    e = datetime.strptime(end, '%Y-%m-%d')
+    start_date = datetime.strptime(start, input_format)
+    end_date = datetime.strptime(end, input_format)
 
-    if e >= s:
-        raise ValueError('end should be no later than start')
+    if end_date < start_date:
+        raise ValueError('end should be no earlier than start')
 
-    rng = []
+    prefixes = []
 
-    if s.year == e.year:
-        if s.month == e.month:
-            month = s.month
-            if (s.day == 1) & (e.day == calendar.monthrange(s.year, month)[1]):
-                rng += [f'{s.year}/{month:02d}']
-            else:
-                days = range(s.day, e.day+1)
-                rng += [f'{s.year}/{month:02d}/{day:02d}' for day in days]
-        elif (s.month == 1) & (s.day == 1) & (e.month == 12) & (e.day == 31):
-            rng += [f'{s.year}']
-        else:
-            _end = datetime(s.year, s.month, calendar.monthrange(s.year, s.month)[1]).strftime('%Y-%m-%d')
-            rng += date_range_prefix(start=start, end=_end)
-            
-            if e.month - s.month > 1:
-                rng += [f'{s.year}/{month:02d}' for month in range(s.month+1, e.month)]
-            
-            _start = datetime(e.year, e.month, 1).strftime('%Y-%m-%d')
-            rng += date_range_prefix(start=_start, end=end)
-    else:
-        rng += date_range_prefix(start, f'{s.year}-12-31')
+    if start_date.year == end_date.year:  # same year for start and end
+        if start_date.month == end_date.month:  # same month for start and end
+            if start_date.day == 1 and is_month_last_day(end_date):  # full month from start to end
+                prefixes += [f'{start_date.year}{output_delimiter}{start_date.month:02d}']
+            else:  # not full month, so list every day
+                days = range(start_date.day, end_date.day + 1)
+                prefixes += [f'{start_date.year}{output_delimiter}{end_date.month:02d}{output_delimiter}{day:02d}'
+                             for day in days]
+        elif start_date.month == 1 and start_date.day == 1 and end_date.month == 12 and end_date.day == 31:  # full year
+            prefixes += [f'{start_date.year}']
+        else:  # same year but across multiple months
+            # get start month prefixes
+            start_month_last_date = datetime(start_date.year,
+                                             start_date.month,
+                                             month_days(start_date.year, start_date.month)
+                                             ).strftime(input_format)
+            prefixes += date_range_prefix(start=start, end=start_month_last_date)
 
-        if s.year != e.year:
-            rng += [f'{year}' for year in range(s.year+1, e.year)]
-        
-        rng += date_range_prefix(f'{e.year}-01-01', end)
-    return rng
+            # get full months prefixes in between start and end
+            if end_date.month > start_date.month + 1:
+                months = range(start_date.month + 1, end_date.month)
+                prefixes += [f'{start_date.year}{output_delimiter}{month:02d}' for month in months]
 
+            # get end month prefixes
+            end_month_first_date = datetime(end_date.year, end_date.month, 1).strftime(input_format)
+            prefixes += date_range_prefix(start=end_month_first_date, end=end)
+    else:  # if start and end across multiple years
+        # get start year prefixes
+        start_year_last_date = datetime(start_date.year, 12, 31).strftime(input_format)
+        prefixes += date_range_prefix(start=start, end=start_year_last_date)
 
-if __name__ == '__main__':
-    cases = [
-        ('2021-01-15', '2021-01-25'),
-        ('2021-01-15', '2021-02-25'),
-        ('2021-01-15', '2021-03-25'),
-        ('2021-01-01', '2021-01-01'),
-        ('2021-01-01', '2021-01-31'),
-        ('2021-01-01', '2021-02-01'),
-        ('2021-01-01', '2021-02-05'),
-        ('2021-01-01', '2021-02-28'),
-        ('2021-01-01', '2021-03-31'),
-        ('2021-12-29', '2022-01-05'),
-        ('2021-12-29', '2023-01-05'),
-        ('2021-12-19', '2020-02-28'),
-    ]
+        # get full years prefixes in between start and end
+        if end_date.year > start_date.year + 1:
+            years = range(start_date.year + 1, end_date.year)
+            prefixes += [f'{year}' for year in years]
 
-    for case in cases:
-        print(*case)
-        print(date_range_prefix(*case), '\n')
+        # get end year prefixes
+        end_year_first_date = datetime(end_date.year, 1, 1).strftime(input_format)
+        prefixes += date_range_prefix(start=end_year_first_date, end=end)
+    return prefixes
--- test.py
+++ test.py
@@ -1,84 +1,35 @@
-import inspect
 from datetime import datetime
 from date_range_prefix import date_range_prefix
-import unittest
+
+import unittest as ut
 
 
-def get_func_name():
-    return inspect.getouterframes(inspect.currentframe())[1].function
-
-
-class TestDateRange(unittest.TestCase):
+class TestDateRange(ut.TestCase):
     CASES = {
-        'test_1': {'type': 'assertEqual', 'kw': {'start':None, 'end':None}, 'res': [datetime.today().strftime('%Y/%m/%d')]},
-        'test_2': {'type': 'assertEqual', 'kw': {'start':'2021-01-05', 'end':None}, 'res': ['2021/01/05']},
-        'test_3': {'type': 'assertEqual', 'kw': {'start':'2021-01-05', 'end':'2021-01-07'}, 'res': ['2021/01/05', '2021/01/06', '2021/01/07']},
-        'test_4': {'type': 'assertEqual', 'kw': {'start':'2021-01-01', 'end':'2021-01-31'}, 'res': ['2021/01']},
-        'test_5': {'type': 'assertEqual', 'kw': {'start':'2021-01-31', 'end':'2021-02-01'}, 'res': ['2021/01/31', '2021/02/01']},
-        'test_6': {'type': 'assertEqual', 'kw': {'start':'2021-01-31', 'end':'2021-02-28'}, 'res': ['2021/01/31', '2021/02']},
-        'test_7': {'type': 'assertEqual', 'kw': {'start':'2021-01-31', 'end':'2021-03-01'}, 'res': ['2021/01/31', '2021/02', '2021/03/01']},
-        'test_8': {'type': 'assertEqual', 'kw': {'start':'2021-01-01', 'end':'2021-03-01'}, 'res': ['2021/01', '2021/02', '2021/03/01']},
-        'test_9': {'type': 'assertEqual', 'kw': {'start':'2021-11-30', 'end':'2022-01-01'}, 'res': ['2021/11/30', '2021/12', '2022/01/01']},
-        'test_10': {'type': 'assertEqual', 'kw': {'start':'2021-11-30', 'end':'2023-01-01'}, 'res': ['2021/11/30', '2021/12', '2022', '2023/01/01']},
-        'test_11': {'type': 'assertEqual', 'kw': {'start':'2021-01-01', 'end':'2021-12-31'}, 'res': ['2021']},
-        'test_12': {'type': 'assertEqual', 'kw': {'start':'2021-01-01', 'end':'2022-01-01'}, 'res': ['2021', '2022/01/01']},
+        'test_01': {'type': 'assertEqual', 'kw': {'start': None, 'end': None}, 'res': [datetime.today().strftime('%Y/%m/%d')]},
+        'test_02': {'type': 'assertEqual', 'kw': {'start': '2021-01-05', 'end': None}, 'res': ['2021/01/05']},
+        'test_03': {'type': 'assertEqual', 'kw': {'start': '2021-01-05', 'end': '2021-01-07'}, 'res': ['2021/01/05', '2021/01/06', '2021/01/07']},
+        'test_04': {'type': 'assertEqual', 'kw': {'start': '2021-01-01', 'end': '2021-01-31'}, 'res': ['2021/01']},
+        'test_05': {'type': 'assertEqual', 'kw': {'start': '2021-01-31', 'end': '2021-02-01'}, 'res': ['2021/01/31', '2021/02/01']},
+        'test_06': {'type': 'assertEqual', 'kw': {'start': '2021-01-31', 'end': '2021-02-28'}, 'res': ['2021/01/31', '2021/02']},
+        'test_07': {'type': 'assertEqual', 'kw': {'start': '2021-01-31', 'end': '2021-03-01'}, 'res': ['2021/01/31', '2021/02', '2021/03/01']},
+        'test_08': {'type': 'assertEqual', 'kw': {'start': '2021-01-01', 'end': '2021-03-01'}, 'res': ['2021/01', '2021/02', '2021/03/01']},
+        'test_09': {'type': 'assertEqual', 'kw': {'start': '2021-11-30', 'end': '2022-01-01'}, 'res': ['2021/11/30', '2021/12', '2022/01/01']},
+        'test_10': {'type': 'assertEqual', 'kw': {'start': '2021-11-30', 'end': '2023-01-01'}, 'res': ['2021/11/30', '2021/12', '2022', '2023/01/01']},
+        'test_11': {'type': 'assertEqual', 'kw': {'start': '2021-01-01', 'end': '2021-12-31'}, 'res': ['2021']},
+        'test_12': {'type': 'assertEqual', 'kw': {'start': '2021-01-01', 'end': '2022-01-01'}, 'res': ['2021', '2022/01/01']},
     }
 
-    def test_1(self):
-        case = self.CASES.get(get_func_name())
-        getattr(self, case['type'])(date_range_prefix(**case['kw']), case['res'])
-    
-    def test_2(self):
-        case = self.CASES.get(get_func_name())
-        getattr(self, case['type'])(date_range_prefix(**case['kw']), case['res'])
-
-    def test_3(self):
-        case = self.CASES.get(get_func_name())
-        getattr(self, case['type'])(date_range_prefix(**case['kw']), case['res'])
-    
-    def test_4(self):
-        case = self.CASES.get(get_func_name())
-        getattr(self, case['type'])(date_range_prefix(**case['kw']), case['res'])
-    
-    def test_5(self):
-        case = self.CASES.get(get_func_name())
-        getattr(self, case['type'])(date_range_prefix(**case['kw']), case['res'])
-
-    def test_6(self):
-        case = self.CASES.get(get_func_name())
-        getattr(self, case['type'])(date_range_prefix(**case['kw']), case['res'])
-    
-    def test_7(self):
-        case = self.CASES.get(get_func_name())
-        getattr(self, case['type'])(date_range_prefix(**case['kw']), case['res'])
-
-    def test_8(self):
-        case = self.CASES.get(get_func_name())
-        getattr(self, case['type'])(date_range_prefix(**case['kw']), case['res'])
-    
-    def test_9(self):
-        case = self.CASES.get(get_func_name())
-        getattr(self, case['type'])(date_range_prefix(**case['kw']), case['res'])
-    
-    def test_10(self):
-        case = self.CASES.get(get_func_name())
-        getattr(self, case['type'])(date_range_prefix(**case['kw']), case['res'])
-    
-    def test_11(self):
-        case = self.CASES.get(get_func_name())
-        getattr(self, case['type'])(date_range_prefix(**case['kw']), case['res'])
-    
-    def test_12(self):
-        case = self.CASES.get(get_func_name())
-        getattr(self, case['type'])(date_range_prefix(**case['kw']), case['res'])
+    def test(self):
+        for case in self.CASES.values():
+            getattr(self, case['type'])(date_range_prefix(**case['kw']), case['res'])
 
     def test_err_1(self):
-        self.assertRaises(AssertionError, date_range_prefix, start='2022-02-01', end='2022-01-01')
+        self.assertRaises(ValueError, date_range_prefix, start='2022-02-01', end='2022-01-01')
 
     def test_err_2(self):
         self.assertRaisesRegex(ValueError, 'day is out of range for month', date_range_prefix, start='2021-02-01', end='2021-02-29')
 
 
 if __name__ == '__main__':
-    unittest.main()
-    
+    ut.main()
